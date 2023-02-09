245b7bed392614f6ae81b0d0756d438cd065d951
Rrezakk <52713544+Rrezakk@users.noreply.github.com>
2023-02-09 13:20:44 +0300

    Initial project and domain layer

---
+++ Market.Domain/Enums/CarType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Market.Domain.Enums;
+
+public enum CarType
+{
+    [Display(Name = "Легковой автомобиль")]
+    PassengerCar =0,
+    [Display(Name = "Спортивный автомобиль")]
+    SportsCar=1,
+    [Display(Name = "Внедорожник")]
+    Suv =2,
+    
+}
